c0176564f6f5a4f475b67ba0d6b5393f5d032483
netanelen <netanel9200@gmail.com>
2025-10-26 01:40:03 +0200

    fix_vms

--- variables.tf
+++ variables.tf
@@ -21,7 +21,7 @@ variable "node_name" {
 variable "template_vm_id" {
   description = "The VM ID of the template to clone"
   type        = number
-  default = 101
+  default = 555
 }
 
 variable "template_node" {
